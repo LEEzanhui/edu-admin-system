197d8eabcc579dab3c4aedad6f943dee8befee6d
LEEzanhui <925972643@qq.com>
2019-12-19 16:51:59 +0800

    CourseDB modify

--- src/Data/CourseDB.java
+++ src/Data/CourseDB.java
@@ -35,8 +35,8 @@ public class CourseDB {
 				return entry.getValue();
 			}
 		}
-		
-		return new Course();
+		return null;
+//		return new Course();
 		//		id from course
 	}
 //		id from teacher	- return the courses he taught
@@ -44,18 +44,44 @@ public class CourseDB {
 	
 	public synchronized Vector<Course> searchByName(String courseName) {
 		int length = courseName.length();
-		Vector<Course> ans = null;
+		if(length <= 3) {
+			return null;
+		}
+		
+		Vector<Course> ans = new Vector<Course>();
 		for (Map.Entry<String, Course> entry : courseBase.entrySet()) {
 			if(entry.getValue().name().substring(0, length) == courseName) {
 				ans.add(entry.getValue());
 			}
 		}
-		
-		return ans;
+		if(ans.size() == 0)
+			return null;
+		else
+			return ans;
 //		fuzzy search
 	}
 	
 	public synchronized void modify(Map<String, Course> in) {
+		Iterator<Map.Entry<String, Course>> en = in.entrySet().iterator();
+		
+		while(en.hasNext()) {
+			Map.Entry<String, Course> entry1 = en.next();
+			Iterator<Map.Entry<String, Course>> index = courseBase.entrySet().iterator();
+			while(index.hasNext()) {
+				Map.Entry<String, Course> entry2 = index.next();
+				
+				if(entry1.getKey() == entry2.getKey()) {
+					if(entry1.getValue().name() == null) {
+						en.remove();
+					}
+					else {
+						courseBase.put(entry1.getKey(), entry1.getValue());
+					}
+				}
+			}
+			
+		}
+		
 //		courseId not exist - new
 //		courseId exist - view Course's flag
 //			update Course or delete it
